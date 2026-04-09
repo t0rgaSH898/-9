32bc3600c34f53ebd6fa7f1feb20286c6444dad2
Владислав Торганенко <vladtorg.anatol@gmail.com>
2026-04-09 11:21:22 +0300

    Update zd3.cpp

--- zd3.cpp
+++ zd3.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    int num, sum = 0;
-    cin >> num;
-    while (num != 0) {
-    sum += num;
-    cin >> num;
-    }    
-    cout << sum;
-        
+    int n;
+    cin >> n;
+
+    if (n == 12 || n == 1 || n == 2) {
+        cout << "Зима" << endl;
+    } else if (n == 3 || n == 4 || n == 5) {
+        cout << "Весна" << endl;
+    } else if (n == 6 || n == 7 || n == 8) {
+        cout << "Лето" << endl;
+    } else if (n == 9 || n == 10 || n == 11) {
+        cout << "Осень" << endl;
+    } else {
+        cout << "Ошибка" << endl;
+    }
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
