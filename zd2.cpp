764e304820ee68c6be7c3e4f5718525ded35d836
Владислав Торганенко <vladtorg.anatol@gmail.com>
2026-04-09 11:20:39 +0300

    Update zd2.cpp

--- zd2.cpp
+++ zd2.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
     int n;
-    int i;
-    int a = 0;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        a += i;
-}
-    cout << a << endl;
-    
+
+    if (n == 1) cout << "Понедельник" << endl;
+    else if (n == 2) cout << "Вторник" << endl;
+    else if (n == 3) cout << "Среда" << endl;
+    else if (n == 4) cout << "Четверг" << endl;
+    else if (n == 5) cout << "Пятница" << endl;
+    else if (n == 6) cout << "Суббота" << endl;
+    else if (n == 7) cout << "Воскресенье" << endl;
+    else cout << "Неверный день" << endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
