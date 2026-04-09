19cb9fdcc89aaa41c4cb2adda3d1b77a681a38f6
Владислав Торганенко <vladtorg.anatol@gmail.com>
2026-04-09 11:22:01 +0300

    Update zd4.cpp

--- zd4.cpp
+++ zd4.cpp
@@ -2,16 +2,45 @@
 using namespace std;
 
 int main() {
-    int N;
-    
-    cin >> N;
-    
-    for (int i = 1; i <= N; i++) {
-        if (i % 2 == 0) {
-            cout << i << " ";
-        }
+    setlocale(LC_ALL, "Russian");
+
+    char op;
+    int a, b;
+    cin >> op >> a >> b;
+
+    switch (op) {
+        case '+':
+            cout << a + b << endl;
+            break;
+        case '-':
+            cout << a - b << endl;
+            break;
+        case '*':
+            cout << a * b << endl;
+            break;
+        case '/':
+            if (b == 0) {
+                cout << "Деление на ноль" << endl;
+            } else {
+                cout << a / b << endl;
+            }
+            break;
+        default:
+            cout << "Ошибка" << endl;
     }
-    
-    cout << endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
